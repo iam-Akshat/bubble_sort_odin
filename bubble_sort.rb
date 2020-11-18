670f51c091b20cdae1c58822fe0b7bdf5a2063b6
unknown <rayodo667@gmail.com>
2020-11-18 11:05:39 +0100

    fixed linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,36 +1,36 @@
-def bubble_sort (array)
-    a = array.length
-    
-    loop do 
-        swapped = false
-        (a-1).times do |i|
-            if array[i] > array[i + 1]
-                array[i], array[i + 1] = array[i + 1], array[i]
-                swapped = true
-            end
+def bubble_sort(array)
+  a = array.length
+
+  loop do
+    swapped = false
+      (a - 1).times do |i|
+        if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
         end
-        break if not swapped
-    end
-end   
+      end
+    break unless swapped
+  end
+end
 array = [156, 2000, 80, 14, 40, 20, 3, 7]
-bubble_sort (array)
+bubble_sort(array)
 puts array.inspect
 
 def bubble_sort_by(array)
-    didChange=nil
-    for t in 0...array.length
-        for y in 0...array.length-t-1
-            if yield(array[y],array[y+1])>0
-                array[y],array[y+1]=array[y+1],array[y]
-                didChange=true
-            end    
-        end
-        didChange or break
-        didChange=nil
-    end
+  didChange=nil
+  for t in 0...array.length
+      for y in 0...array.length-t-1
+          if yield(array[y],array[y+1])>0
+              array[y],array[y+1]=array[y+1],array[y]
+              didChange=true
+          end    
+      end
+      didChange or break
+      didChange=nil
+  end
 end    
 arrString=["hi","hello","hey","h"]
 bubble_sort_by(arrString) do |left,right|
-   left.length - right.length
+  left.length - right.length
 end
 puts arrString.inspect
